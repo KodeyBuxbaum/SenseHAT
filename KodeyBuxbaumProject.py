4315bf6de425e3757b84b438b725e019c2d5c9ae
lbryant654 <lbryant654@gmail.com>
2018-12-13 13:35:37 -0500

    4th edit, will try to get vegetables to appear tommorrow

--- KodeyBuxbaumProject.py
+++ KodeyBuxbaumProject.py
@@ -4,16 +4,15 @@ from random import *
 sense = SenseHat()
 
 #variables here-----------------------------------------
-cyan = (0, 255, 239)
+direction = "right"
 purple = (205, 0, 255)
-red = (255, 0, 0)
 blank = (0, 0, 0)
 slug = [[3,4], [4,4], [5, 4]]
 sense.set_pixel(2, 4, blank)
-sense.set_pixel(3, 4, red)
+sense.set_pixel(3, 4, purple)
 sense.set_pixel(4, 4, purple)
-direction = "right"
-
+veggie_color = (255, 255, 255)
+vegetables =[]
 
 #functions here-------------------------
 def draw_slug():
@@ -25,23 +24,91 @@ def move():
     first = slug[0]
     next = list(last)
     if direction == "right":
-        next[0] = last[0] + 1
         if last[0] + 1 == 8:
             next[0] = 0
         else:
             next[0] = last[0] + 1
+    elif direction == "left":
+        if last[0] - 1 == -1:
+            next[0] = 7
+        else:
+            next[0] = last[0] - 1
+    elif direction == "down":
+        if last[1] + 1 == 8:
+            next[1] = 0
+        else:
+            next[1] = last[1] + 1
+    elif direction == "up":
+        if last[1] - 1 == -1:
+            next[1] = 7
+        else:
+            next[1] = last[1] - 1
 
         
     sense.set_pixel(next[0], next[1], purple)
     sense.set_pixel(first[0], first[1], blank)
     slug.append(next)
     slug.remove(first)
-        
+
+
+
+def joystick_moved(event):
+    global direction
+    direction = event.direction
+
+
+def make_veg():
+    x = randint(0, 7)
+    y = randint(0, 7)
+    new = [x, y]
+    while new in slug:
+        x = randint(0, 7)
+        y = randint(0, 7)
+        new = [x, y, veggie_color]
+        if len(vegetables) < 3:
+            make_veg()
+        vegetables.append(new)
 #main program here----------------------------------
 sense.clear()
+
 while True:
-    draw_slug()
     sleep(0.5)
+    draw_slug()
     move()
+    sense.stick.direction_any = joystick_moved
+    make_veg()
+    sense.set_pixel
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
     
